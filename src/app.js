68408c2454e3ee5eaf94197da74fa50da8d4a326
Mukul Mehra <mehramukul904@gmail.com>
2024-12-04 14:37:58 +0530

    All Setup configured

--- src/app.js
+++ src/app.js
@@ -16,5 +16,13 @@ app.use(express.urlencoded({
 }))
 app.use(express.static("public"))
 app.use(cookieParser())
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes decleration
+
+app.use("/api/v1/users" , userRouter)
+
 
 export {app}
